4dca8a0b87c1cac604d3ae022ec16d46551c3eaa
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-08-26 14:34:45 +0200

    we

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { Votes, VoteType } from "../../types/votes";
-import CafeInfo from "../CafeInfoGG/CafeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import { VoteOptions } from "../VoteOptions/VoteOptions";
 import { VoteStats } from "../VoteStats/VoteStats";
 import css from "./App.module.css";
